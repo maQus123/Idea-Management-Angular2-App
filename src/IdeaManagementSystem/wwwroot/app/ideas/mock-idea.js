9066536cc6902d1846b32fa0538ec92e5602289a
maQus123 <markus@lman.eu>
2016-03-30 19:58:22 +0200

    deleted dummy data

--- src/IdeaManagementSystem/wwwroot/app/ideas/mock-idea.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.IDEAS = [
-    { id: "001", title: "Geländer XYZ", description: "sadflja sdfölaj sdföla sdfasdfa", category: "Produktion", isSecret: false },
-    { id: "002", title: "PowerPoint 16:9", description: "asdf asdfa sdfa09t 0943uj ag", category: "IT", isSecret: false },
-    { id: "003", title: "TK-Pizza Konfigurator", description: "adfkajsdfölasj dfpa9sdjfaölsd", category: "Innovation", isSecret: true },
-    { id: "004", title: "Bödsinnige Idee", description: "asdfajsdfölsaj dfölaj sdfaj sfd", category: "IT", isSecret: true },
-    { id: "005", title: "Bla Bla Car 123", description: "asdflja sdölfj asöldfj asaöse aöjklsf", category: "Innovation", isSecret: false }
-];
-//# sourceMappingURL=mock-idea.js.map
